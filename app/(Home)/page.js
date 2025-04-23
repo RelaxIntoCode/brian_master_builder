0b1311d5e93c3f4eb0654f997574295bc9ec9c15
RelaxIntoCode <jayow1991@gmail.com>
2025-04-23 10:20:02 +1000

    Add components folder, rearrange HomePage

---
+++ app/(Home)/page.js
@@ -0,0 +1,11 @@
+import styles from "./homePage.module.css";
+import Navbar from "@/components/Navbar/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Home Page</h1>
+    </div>
+  );
+}
---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <nav>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+    </nav>
+  );
+}
+
+export default Navbar;
