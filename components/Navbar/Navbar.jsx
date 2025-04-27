751153e7c5d6b6c9c7eda8cabadae4b59ab9be28
RelaxIntoCode <jayow1991@gmail.com>
2025-04-27 13:14:14 +1000

    Add initial styling for links dropdown in Navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -2,9 +2,33 @@
 import styles from "./Navbar.module.css";
 import Link from "next/link";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { useState } from "react";
+
 function Navbar() {
+  const [showLinks, setShowLinks] = useState(false);
   const pathname = usePathname();
+  const router = useRouter();
+  const links = [
+    {
+      id: 1,
+      link: "/contact",
+      title: "Contact Us",
+    },
+    {
+      id: 2,
+      link: "/about-us",
+      title: "About Us",
+    },
+  ];
+
+  function handleChange(e) {
+    const selectedLink = e.target.value;
+    if (selectedLink) {
+      router.push(selectedLink);
+    }
+  }
   return (
     <nav className={styles.nav}>
       <div className={styles.navSection}>
@@ -20,7 +44,7 @@ function Navbar() {
         </Link>
       </div>
       <div className={styles.navSection}>
-        <div className={styles.navLink}>
+        {/* <div className={styles.navLink}>
           <Link
             href="/contact"
             className={pathname === "/contact" ? styles.active : ""}
@@ -33,6 +57,43 @@ function Navbar() {
           >
             About Us
           </Link>
+        </div> */}
+        {/* <div>
+          <select name="" id="" onChange={handleChange}>
+            <option value="">Links</option>
+            {links.map((link) => {
+              return (
+                <option key={link.id} value={link.link}>
+                  {link.title}
+                </option>
+              );
+            })}
+          </select>
+        </div> */}
+        <div
+          className={styles.linksContainer}
+          onClick={() => setShowLinks(!showLinks)}
+        >
+          <p>Links</p>
+          <span>
+            <MdOutlineKeyboardArrowDown
+              className={`${showLinks ? styles.rotate : ""}`}
+            />
+          </span>
+        </div>
+
+        <div
+          className={`${styles.dropdown} ${showLinks ? styles.translate : ""}`}
+        >
+          <div>
+            <p>Contact Us</p>
+          </div>
+          <div>
+            <p>About Us</p>
+          </div>
+          <div>
+            <p>Gallery</p>
+          </div>
         </div>
       </div>
       <div className={styles.navSection}>
